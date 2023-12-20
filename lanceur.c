49691a1589a8f8def17542b19d3c4c573392924e
Odeez24 <florent.lry24@gmail.com>
2023-12-20 21:52:51 +0100

    V 0.2.1

--- lanceur.c
+++ lanceur.c
@@ -25,7 +25,6 @@
 #define TUBE_RES "TUBE_RES_CLIENT_"
 #define TUBE_ERR "TUBE_ERR_CLIENT_"
 
-
 //--- Outils pour les threads --------------------------------------------------
 /*
  * Les arguments transmis au thread.
@@ -35,7 +34,7 @@ struct my_thread_args {
 };
 
 // run: fonction de lancement du thread
-void * run(struct my_thread_args* a);
+void *run(struct my_thread_args *a);
 
 typedef void *(*start_routine_type)(void *);
 
@@ -50,133 +49,137 @@ void mafct(int sig);
 int main(void) {
   int errnum;
   work = true;
-  if (create_file_sync() == -1){
+  if (create_file_sync() == -1) {
     fprintf(stderr, "Error during create_file_sync");
     exit(EXIT_FAILURE);
   }
   int nbth = 0;
-	struct sigaction act;
+  struct sigaction act;
   act.sa_handler = mafct;
-	act.sa_flags = 0;
-	sigemptyset(&act.sa_mask);
+  act.sa_flags = 0;
+  sigemptyset(&act.sa_mask);
   sigaction(SIGINT, &act, NULL);
   pthread_t th;
-  while (work){
+  while (work) {
     pid_t p = defiler();
-    struct my_thread_args* a = malloc(sizeof(struct my_thread_args));
+    struct my_thread_args *a = malloc(sizeof(struct my_thread_args));
     if (a == NULL) {
       fprintf(stderr, "Error: malloc");
       exit(EXIT_FAILURE);
     }
     a->client = p;
-    if ((errnum = pthread_create(&th, NULL, (start_routine_type)run, a)) != 0) {
-        fprintf(stderr, "pthread_create: %s\n", strerror(errnum));
-        exit(EXIT_FAILURE);
+    if ((errnum
+          = pthread_create(&th, NULL, (start_routine_type) run, a)) != 0) {
+      fprintf(stderr, "pthread_create: %s\n", strerror(errnum));
+      exit(EXIT_FAILURE);
     }
     ++nbth;
   }
-  for (int i = 0; i < nbth; ++i){
+  for (int i = 0; i < nbth; ++i) {
     int *ptr;
     if ((errnum = pthread_join(th, (void **) &ptr)) != 0) {
-        fprintf(stderr, "pthread_join: %s\n", strerror(errnum));
-        exit(EXIT_FAILURE);
+      fprintf(stderr, "pthread_join: %s\n", strerror(errnum));
+      exit(EXIT_FAILURE);
     }
     free(ptr);
   }
-  if (destroy_file() == -1){
+  if (destroy_file() == -1) {
     fprintf(stderr, "Error during destroy_file");
     exit(EXIT_FAILURE);
   }
   exit(EXIT_SUCCESS);
 }
 
-void *run(struct my_thread_args* a) {
+void *run(struct my_thread_args *a) {
   int fd;
   char pid[101];
   int pidlen;
-  if ((pidlen = snprintf(pid, UCHAR_MAX, "%d", a->client)) < 0 ||
-    pidlen > 100){
+  if ((pidlen = snprintf(pid, UCHAR_MAX, "%d", a->client)) < 0
+      || pidlen > 100) {
     return NULL;
   }
   pid[100] = '\0';
-  char tube_cl[(int)strlen(TUBE_CL) + pidlen];
-  strcpy (tube_cl,TUBE_CL);
-  strncat(tube_cl, pid, (size_t)pidlen);
-  char tube_res[(int)strlen(TUBE_RES) + pidlen];
-  strcpy (tube_res,TUBE_RES);
-  strncat(tube_res, pid, (size_t)pidlen);
-  char tube_err[(int)strlen(TUBE_ERR) + pidlen];
-  strcpy (tube_err,TUBE_ERR);
-  strncat(tube_err, pid, (size_t)pidlen);
-  switch(fork()){
+  char tube_cl[(int) strlen(TUBE_CL) + pidlen];
+  strcpy(tube_cl, TUBE_CL);
+  strncat(tube_cl, pid, (size_t) pidlen);
+  char tube_res[(int) strlen(TUBE_RES) + pidlen];
+  strcpy(tube_res, TUBE_RES);
+  strncat(tube_res, pid, (size_t) pidlen);
+  char tube_err[(int) strlen(TUBE_ERR) + pidlen];
+  strcpy(tube_err, TUBE_ERR);
+  strncat(tube_err, pid, (size_t) pidlen);
+  switch (fork()) {
     case -1:
       return NULL;
     case 0:
-      if ((fd = open(tube_cl, O_RDONLY)) == -1){
-    return NULL;
-  }
-  char c;
-  char cmd[40];
-  char opt[100];
-  int i = 0;
-  while ((c = (char)read(fd, &c, sizeof(char))) > 0 && c != '-'){
-    if (c == -1) {
-      return NULL;
-    }
-    cmd[i] = c;
-    ++i;
-  }
-  cmd[i] = '\0';
-  i = 0;
-  while ((c = (char)read(fd, &c, sizeof(char))) > 0){
-    if (c == -1) {
-      return NULL;
-    }
-    opt[i] = c;
-    ++i;
-  }
-  opt[i] = '\0';
-  if (close(fd) == -1){
-    return NULL;
-  }
+      if ((fd = open(tube_cl, O_RDONLY)) == -1) {
+        return NULL;
+      }
+      char c;
+      char cmd[40];
+      char opt[100];
+      int i = 0;
+      while ((c = (char) read(fd, &c, sizeof(char))) > 0 && c != '-') {
+        if (c == -1) {
+          return NULL;
+        }
+        cmd[i] = c;
+        ++i;
+      }
+      cmd[i] = '\0';
+      i = 0;
+      while ((c = (char) read(fd, &c, sizeof(char))) > 0) {
+        if (c == -1) {
+          return NULL;
+        }
+        opt[i] = c;
+        ++i;
+      }
+      opt[i] = '\0';
+      if (close(fd) == -1) {
+        return NULL;
+      }
       int fd_res;
       int fd_err;
-      if ((fd_res = open(tube_res, O_WRONLY) == -1)){
+      if ((fd_res = open(tube_res, O_WRONLY) == -1)) {
         return NULL;
       }
-      if ((fd_err = open(tube_err, O_WRONLY) == -1)){
+      if ((fd_err = open(tube_err, O_WRONLY) == -1)) {
         return NULL;
       }
-      if (dup2(fd_res, STDOUT_FILENO) == -1){
+      if (dup2(fd_res, STDOUT_FILENO) == -1) {
         return NULL;
       }
-      if (close(fd_res) == -1){
+      if (close(fd_res) == -1) {
         return NULL;
       }
-      if (dup2(fd_err, STDERR_FILENO) == -1){
+      if (dup2(fd_err, STDERR_FILENO) == -1) {
         return NULL;
       }
-      if (close(fd_err) == -1){
+      if (close(fd_err) == -1) {
         return NULL;
       }
-      execvp(cmd, (char * const *)opt);
+      execvp(cmd, (char * const *) opt);
       fprintf(stderr, "Error during the execution of the command");
       exit(EXIT_FAILURE);
     default:
       break;
   }
-  if (wait(NULL) == -1){
+  if (wait(NULL) == -1) {
     return NULL;
   }
   if (unlink(tube_res) == -1) {
-      return NULL;
+    return NULL;
   }
-  if (unlink(tube_err) == -1){
+  if (unlink(tube_err) == -1) {
     return NULL;
   }
   return 0;
 }
 
-void mafct (int sig) {
-	if (sig == SIGINT) {work = false;}
+void mafct(int sig) {
+  if (sig == SIGINT) {
+    work = false;
+  }
 }
+
